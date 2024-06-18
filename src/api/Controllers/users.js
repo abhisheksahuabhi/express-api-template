107daab302b67959bc235c4fe9fba0f2ed2fafc5
Abhishek Sahu <60737797+abhisheksahuabhi@users.noreply.github.com>
2024-06-18 08:29:25 +0000

    update the code for get user by id

--- src/api/Controllers/users.js
+++ src/api/Controllers/users.js
@@ -1,4 +1,5 @@
-const User = require("../Models/users.js")
+const User = require("../Models/users.js");
+
 async function registerUser(req, res) {
     const body = req.body;
     const newUser = await User.create({
@@ -10,14 +11,27 @@ async function registerUser(req, res) {
         password: body.password,
     })
 
-    return res.status(201).json({ msg: "success", id: newUser._id, users: req.body });
+    return res.status(201).json({ msg: "success", users: newUser});
 }
 
-async function getAllUser(req, res){
-    const fetchAllUsers=await User.find({});
-    return res.json(fetchAllUsers);
+async function getAllUser(req, res) {
+    const fetchAllUsers = await User.find({});
+    return res.status(200).json(fetchAllUsers);
+}
+
+async function getUserById(req, res) {
+    try{
+        const userId=req.params.id;
+        const fetchUsers = await User.findById(userId);
+        if(!fetchUsers) return res.status(404).json({error:"User id not found",status:"404"});
+        return res.status(200).json(fetchUsers);
+    }catch(error){
+        return res.status(500).json({error:"Server error",error});
+    }
+    
 }
 module.exports = {
     registerUser,
     getAllUser,
+    getUserById
 }
--- src/api/Routes/users.js
+++ src/api/Routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {registerUser,getAllUser} = require('../Controllers/users');
+const {registerUser,getAllUser,getUserById} = require('../Controllers/users');
 const {handlingErrorMessage} = require('../Middleware/userFieldValidation')
 
 const router = express.Router();
 
 router.route("/").post(handlingErrorMessage(),registerUser).get(getAllUser);
-
+router.route("/:id").get(getUserById)
 module.exports = router;
